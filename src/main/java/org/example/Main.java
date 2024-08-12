c39f0d328d0c7b2c40ccbbc1cddf001a940ea2bb
jivaram <jivaramnbh@gmail.com>
2024-08-12 22:07:37 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+         Student student1 = (Student) context.getBean("Student1");
+         System.out.println(student1);
+        }
+    }
